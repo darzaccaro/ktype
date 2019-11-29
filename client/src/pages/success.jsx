eec5664777f8c889831c883266b9bda990134e48
kade <zaccaro.kade@gmail.com>
2019-11-29 10:54:26 -0500

    re-revert typo fix

--- client/src/pages/success.jsx
+++ client/src/pages/success.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import theme from '../theme';
+import theme from "../theme";
 import useWindowWidth from "../hooks/useWindowWidth";
 export default function Success() {
-    const width = useWindowWidth();
-    return (
-        <div className="anim-fade-in" style={{
-            marginTop: theme.sizes.spacingV1 - 65,
-            width: "100%",
-
-        }}>
-            <div style={{
-                maxWidth:
-                    width < theme.breakpoints.mobile
-                        ? "325px"
-                        : theme.sizes.contentWidth,
-                margin: "0 auto",
-                marginBottom: theme.sizes.spacingV1 - 10,
-                minHeight: 800
-            }}>
-                <h1>Thank your for your submission!</h1>
-                <p>We'll be in touch as soon as possible. I do my best to respond within 1-2 days.</p>
-            </div>
-        </div>
-    );
+  const width = useWindowWidth();
+  return (
+    <div
+      className="anim-fade-in"
+      style={{
+        marginTop: theme.sizes.spacingV1 - 65,
+        width: "100%"
+      }}
+    >
+      <div
+        style={{
+          maxWidth:
+            width < theme.breakpoints.mobile
+              ? "325px"
+              : theme.sizes.contentWidth,
+          margin: "0 auto",
+          marginBottom: theme.sizes.spacingV1 - 10,
+          minHeight: 800
+        }}
+      >
+        <h1>Thank you for your submission!</h1>
+        <p>
+          We'll be in touch as soon as possible. I do my best to respond within
+          1-2 days.
+        </p>
+      </div>
+    </div>
+  );
 }
